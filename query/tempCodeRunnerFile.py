a11b238fd5b848a01332c9c7aa62d51201c802a5
Manjeet <manjeetman38@gmail.com>
2024-10-04 18:06:16 +0530

    query part is done

---
+++ query/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+# query = input('enter query string here: ')
+
+# # split query string and make lowercase
+# # first strip on query, then split
+# query_terms = [term.lower() for term in query.strip().split()]
+
+# print(query_terms)
+
+# get_sorted_order_documents(query_terms)
